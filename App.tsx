7e551905e9bc7b0d2dd7bac74fc0eca2f2fdf894
stepan.rutz <stepan.rutz@gmx.de>
2025-03-15 14:53:16 +0100

    first

--- App.tsx
+++ App.tsx
@@ -2,12 +2,27 @@ import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
 
 import './global.css';
+import { View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Query, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { SearchPage } from 'components/pages/SearchPage';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator()
+const client = new QueryClient()
 
 export default function App() {
-  return (
-    <>
-      <ScreenContent title="Home" path="App.tsx" />
-      <StatusBar style="auto" />
-    </>
-  );
+    return (
+        <QueryClientProvider client={client}>
+            <NavigationContainer>
+                <Stack.Navigator
+                    screenOptions={{
+                        headerShown: false // Hide default header since we're using our custom StatusBar
+                    }}>
+                    <Stack.Screen name="Home" component={SearchPage} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </QueryClientProvider>
+    )
 }
---
+++ components/pages/SearchPage.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export function SearchPage() {
+    return (
+        <SafeAreaView className="flex-1 bg-green-300">
+            <Text>Soewfoije</Text>
+        </SafeAreaView>
+    )
+}
